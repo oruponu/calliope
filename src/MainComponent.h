017183bd92beea487e375ccc0257051692d6cc56
oruponu <oruponu@outlook.com>
2026-02-11 22:50:28 +0900

    テストシーケンスの再生を実装

--- src/MainComponent.h
+++ src/MainComponent.h
@@ -1,15 +1,27 @@
 #pragma once
 
-#include <juce_audio_utils/juce_audio_utils.h>
+#include "audio/MidiDeviceOutput.h"
+#include "engine/PlaybackEngine.h"
+#include "model/MidiSequence.h"
+#include <juce_gui_extra/juce_gui_extra.h>
 
 class MainComponent : public juce::Component
 {
 public:
     MainComponent();
+    ~MainComponent() override;
 
     void paint(juce::Graphics&) override;
     void resized() override;
 
 private:
+    void buildTestSequence();
+
+    MidiSequence sequence;
+    PlaybackEngine playbackEngine;
+    MidiDeviceOutput midiOutput;
+
+    juce::TextButton playButton{"Play"};
+
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MainComponent)
 };
