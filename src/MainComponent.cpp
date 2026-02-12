b106e3a9f0c990bfea7021968b414669327dbbfd
oruponu <oruponu@outlook.com>
2026-02-13 02:07:52 +0900

    起動時のテストノートを削除

--- src/MainComponent.cpp
+++ src/MainComponent.cpp
@@ -47,8 +47,6 @@ MainComponent::MainComponent()
 {
     midiOutput.open();
 
-    buildTestSequence();
-
     playbackEngine.setSequence(&sequence);
     playbackEngine.addListener(&midiOutput);
 
@@ -331,19 +329,4 @@ void MainComponent::onSequenceLoaded()
 
     int c4Y = PianoRollComponent::headerHeight + (127 - 60) * PianoRollComponent::noteHeight - getHeight() / 2;
     viewport.setViewPosition(0, c4Y);
-}
-
-void MainComponent::buildTestSequence()
-{
-    auto& track = sequence.addTrack();
-
-    // C major scale
-    track.addNote({60, 100, 0, 480});    // C4
-    track.addNote({62, 100, 480, 480});  // D4
-    track.addNote({64, 100, 960, 480});  // E4
-    track.addNote({65, 100, 1440, 480}); // F4
-    track.addNote({67, 100, 1920, 480}); // G4
-    track.addNote({69, 100, 2400, 480}); // A4
-    track.addNote({71, 100, 2880, 480}); // B4
-    track.addNote({72, 100, 3360, 480}); // C5
 }
