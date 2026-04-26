c75d4665d5bc5a672cbd09dc1cb03e087b8167e4
oruponu <oruponu@outlook.com>
2026-04-26 18:09:35 +0900

    VSTプラグインへMIDIメッセージを送信

--- src/audio/VstPluginHost.h
+++ src/audio/VstPluginHost.h
@@ -2,13 +2,14 @@
 
 #include "../engine/PlaybackEngine.h"
 #include <juce_audio_processors/juce_audio_processors.h>
+#include <juce_audio_utils/juce_audio_utils.h>
 
 class VstPluginHost : public PlaybackEngine::Listener
 {
 public:
     VstPluginHost();
 
-    void prepare(juce::AudioProcessorGraph& graph);
+    void prepare(juce::AudioProcessorGraph& graph, juce::AudioProcessorPlayer& player);
     bool loadPlugin(const juce::File& file);
 
     void onNoteOn(int trackIndex, const MidiNote& note) override;
@@ -18,6 +19,7 @@ public:
 private:
     juce::AudioPluginFormatManager formatManager;
     juce::AudioProcessorGraph* graph = nullptr;
+    juce::AudioProcessorPlayer* audioPlayer = nullptr;
     juce::AudioProcessorGraph::NodeID midiInNodeId;
     juce::AudioProcessorGraph::NodeID audioOutNodeId;
     juce::AudioProcessorGraph::NodeID pluginNodeId;
