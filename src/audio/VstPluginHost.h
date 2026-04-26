a3eb33b9734ce1435b3fe28e806655022aea2661
oruponu <oruponu@outlook.com>
2026-04-26 17:22:30 +0900

    VSTプラグインのロード処理を実装

--- src/audio/VstPluginHost.h
+++ src/audio/VstPluginHost.h
@@ -1,11 +1,24 @@
 #pragma once
 
 #include "../engine/PlaybackEngine.h"
+#include <juce_audio_processors/juce_audio_processors.h>
 
 class VstPluginHost : public PlaybackEngine::Listener
 {
 public:
+    VstPluginHost();
+
+    void prepare(juce::AudioProcessorGraph& graph);
+    bool loadPlugin(const juce::File& file);
+
     void onNoteOn(int trackIndex, const MidiNote& note) override;
     void onNoteOff(int trackIndex, const MidiNote& note) override;
     void onMidiEvent(int trackIndex, const MidiEvent& event) override;
+
+private:
+    juce::AudioPluginFormatManager formatManager;
+    juce::AudioProcessorGraph* graph = nullptr;
+    juce::AudioProcessorGraph::NodeID midiInNodeId;
+    juce::AudioProcessorGraph::NodeID audioOutNodeId;
+    juce::AudioProcessorGraph::NodeID pluginNodeId;
 };
